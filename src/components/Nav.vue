b2e22fd30e413ff160038821cd1f354f66fcf643
Seemab Hassan 00470150 <seemab.hassan1@huawei.com>
2020-12-14 18:15:27 +0500

    Navbar fix. Tooltip disable on mini-expansion

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -82,7 +82,7 @@
             <v-list-item-icon v-if=!mini>
                       <v-icon>{{ item.action }}</v-icon>
                     </v-list-item-icon>
-            <v-list-item-content >
+            <v-list-item-content>
               <v-list-item-title v-text="item.title"></v-list-item-title>
             </v-list-item-content>
           </template>
